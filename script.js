0e033124459ca8cdd52b877fd782ca98185cd1cb
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2023-10-13 10:07:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 function decimalToBinary(decimal) {
   if (decimal === 0) {
-        return "0";
-    }
+    return '0';
+  }
 
-    let binary = "";
-    while (decimal > 0) {
-        let remainder = decimal % 2;
-        binary = remainder + binary;
-        decimal = Math.floor(decimal / 2);
-    }
+  let binary = '';
+  while (decimal > 0) {
+    binary = (decimal % 2) + binary;
+    decimal = Math.floor(decimal / 2);
+  }
 
-    return binary;
+  return binary;
 }
 
 // Input examples
 const input1 = 7;
 
 // Output examples
-//const output1 = decimalToBinary(input1);
+const output1 = decimalToBinary(input1);
 
-window.decimalToBinary = decimalToBinary(input1);
+console.log(output1);
